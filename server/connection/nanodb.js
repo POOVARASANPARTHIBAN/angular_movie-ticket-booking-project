5672cb1bc9243c79399d889799f849bcb3d23deb
Poovarasan P <poov3028@global.chainsys.com>
2022-06-13 16:37:14 +0530

    hotspot error

--- server/connection/nanodb.js
+++ server/connection/nanodb.js
@@ -1,4 +1,8 @@
 const nano = require("nano");
+let app = express();
+app.disable("x-powered-by");
+let helmet = require("helmet");
+app.use(helmet.hidePoweredBy());
 const url =
   "https://apikey-v2-2mxwaz89u58vkezj2e5jfc41xn3komuaq1j49fhhmu8p:58de0ca6ebd4250a97d0a7d300191f68@75c481c7-3349-4ad5-86c0-311dd22187eb-bluemix.cloudantnosqldb.appdomain.cloud/";
 const nanodb = nano(url);
